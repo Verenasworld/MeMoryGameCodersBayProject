3ea4fe1bf2ff37d23202b9127d56f87cf74a4cf1
verenasworld <verena.felizidas@gmail.com>
2021-12-18 21:23:00 +0100

    refactoring if else blocks for clean code

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFilppedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard(){
+    if(lockBoard) return;
+    if(this === firstCard)return;
+
     this.classList.add('flip');
     if (!hasFilppedCard){
         //first click
@@ -12,7 +16,6 @@ function flipCard(){
         firstCard = this;
         return;
     }//second click
-        hasFilppedCard =false;
         secondCard = this;
         checkForMatch();
 }
@@ -20,20 +23,32 @@ function checkForMatch(){
     // match with data -framework set it in html on each memory card
     let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
     isMatch ? disableCard() : unflipCard();
+
 }
 
 function disableCard(){
     //it is a match - remove flipCard event
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard)
+    resetBoard();
+    console.log("yes");
 }
 
 function unflipCard(){
+    lockBoard = true;
     //not a match
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        lockBoard = false;
+        resetBoard();
+        console.log("not")
     },1100);
+}
+
+function resetBoard(){
+    [hasFilppedCard,lockBoard] = [false,false];
+    [firstCard,secondCard] =[null,null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
